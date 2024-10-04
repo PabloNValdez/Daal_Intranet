8d25ecc1dfe6046e219cc786bb0daf8ff019a3fb
Pablo Valdez <pablovaldez0301@gmail.com>
2024-10-04 16:20:53 -0300

    Fix archivos

--- NewApp/pruebapdf.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-use Fpdf\Fpdf;
-
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial', 'B', 16);
-$pdf->Cell(40, 10, '¡Hola, Mundo!');
-$pdf->Output('I', 'hola_mundo.pdf');
